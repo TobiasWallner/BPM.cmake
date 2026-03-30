12e98a7c2b16004c8e886658065ab3af7570c754
Tobias Wallner <tobias.wallner1@gmx.net>
2026-03-30 17:51:49 +0200

    added test repositories

---
+++ tests/add_install_exact_version/main.cpp
@@ -0,0 +1,17 @@
+#include <libA.hpp>
+#include <iostream>
+int main(){
+    std::string expected_version = "1.2.3";
+
+    std::string libA_version = libA::version();
+
+    std::cout << "expected: " << expected_version << std::endl;
+    std::cout << "got: " << libA_version << std::endl;
+    if(expected_version == libA_version){
+        std::cout << "PASSED" << std::endl;
+        return 0;
+    }else{
+        std::cout << "FAILED" << std::endl;
+        return -1;
+    }
+}
